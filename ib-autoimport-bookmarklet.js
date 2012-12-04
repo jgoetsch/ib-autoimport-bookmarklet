c1949d1e47c889dbab6d31f1e4e21a8c9b93e78b
Jeremy Goetsch <jeremygoetsch@gmail.com>
2012-12-04 13:21:22 -0800

    hostname change

--- ib-autoimport-bookmarklet.js
+++ ib-autoimport-bookmarklet.js
@@ -1,4 +1,4 @@
-if (location.href.substr(0,34) != 'https://www.interactivebrokers.com')
+if (!location.hostname.match(/interactivebrokers.com$/))
   alert('This bookmarklet script must be run from the Account Management page on the Interactive Brokers website');
 else (function(e,a,g,h,f,c,b,d){if(!(f=e.jQuery)||g>f.fn.jquery||h(f)){c=a.createElement("script");c.type="text/javascript";c.src="http://ajax.googleapis.com/ajax/libs/jquery/"+g+"/jquery.min.js";c.onload=c.onreadystatechange=function(){if(!b&&(!(d=this.readyState)||d=="loaded"||d=="complete")){h((f=e.jQuery).noConflict(1),b=1);f(c).remove()}};a.documentElement.childNodes[0].appendChild(c)}})(window,document,"1.8.1",function($,L){
 
